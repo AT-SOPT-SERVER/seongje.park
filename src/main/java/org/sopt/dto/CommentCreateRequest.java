243c90410182bf9a93975afb40f0ad00563b2abd
rookie <piresj0318@gmail.com>
2025-05-25 12:01:39 +0900

    feat: 댓글 300최대 300자 ì 조건 추가

--- src/main/java/org/sopt/dto/CommentCreateRequest.java
+++ src/main/java/org/sopt/dto/CommentCreateRequest.java
@@ -1,6 +1,15 @@
 package org.sopt.dto;
 
-public record CommentCreateRequest(String content) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
 
+public record CommentCreateRequest(
+	@Size(max = 300)
+	String content
+
+	// 문자열 제한 할때는 Size
+	// 숫자 제한 할때는 Max
+
+) {
 
 }
